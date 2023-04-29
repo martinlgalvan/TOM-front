e86efc566d112e63f0add33d710e4dea0e9a31c1
martinlgalva <martinlgalvan@yahoo.com>
2023-04-28 22:15:43 -0300

    s

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ function Logo(){
     return (
         <div className='row justify-content-center align-items-center y-2'>
             <h1 className="d-none">TOM</h1>
-            <img className="img-fluid Largo text-center my-5 pt-5 pb-3" src="/src/assets/img/logo.png" alt="TOM" />
+            <img className="img-fluid Largo text-center my-5 pt-5 pb-3" src={img.default} alt="TOM" />
         </div>
     )
 }
