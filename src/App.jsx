1e119a39e18b8928162207a515b34c626f50016b
martinlgalva <martinlgalvan@yahoo.com>
2023-05-01 17:33:41 -0300

    rollback antes del spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// Prime react styles  
+// Prime react styles
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";   
 import 'primereact/resources/primereact.css';    
