0cab1e5e40236e5e1e77e57c59831a7bef52e69e
martinlgalva <martinlgalvan@yahoo.com>
2023-05-01 17:12:35 -0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// Prime react styles
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
+// Prime react styles  
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";   
 import 'primereact/resources/primereact.css';    
