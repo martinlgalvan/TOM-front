81eedf738c919f529c666c8970610cc6ddd23db0
martinlgalva <martinlgalvan@yahoo.com>
2023-05-03 02:46:49 -0300

    s

--- src/pages/coach/UserRoutineEditPage.jsx
+++ src/pages/coach/UserRoutineEditPage.jsx
@@ -60,19 +60,29 @@ function UserRoutineEditPage(){
 
     //Routine - API
     useEffect(() => {
-        setLoading(true)
-        WeekService.findRoutineByUserId(id)
-            .then(data => {   
-                setRoutine(data)
-                setWeekNumber(data.length + 1)
-                if(data.length == 0){
-                    setCopyWeek(false)
-                } else if(data.length > 0){
-                    setCopyWeek(true)
-                }
-                
-                setLoading(false)
-            })
+        const showLoadingToast = () => {
+            toast.promise(
+                new Promise((resolve,reject) => {
+                    WeekService.findRoutineByUserId(id)
+                        .then(data => {   
+                            setRoutine(data)
+                            setWeekNumber(data.length + 1)
+                            if(data.length == 0){
+                                setCopyWeek(false)
+                            } else if(data.length > 0){
+                                setCopyWeek(true)
+                            }
+                            resolve()
+                            setLoading(false)
+                        })
+                }),
+                {pending: "Loading",
+                success: "Cargó con exito"},
+                {}
+            )
+        }
+
+        
     }, [status])
 
 
@@ -174,8 +184,8 @@ function UserRoutineEditPage(){
               }
     
             }
-            const load = toast.loading("Please wait...")
-        const showLoadingToast = () => {
+            //const load = toast.loading("Please wait...")
+        /*const showLoadingToast = () => {
             toast.promise(
                 new Promise((resolve,reject) => {
                     if(loading != true){
@@ -186,7 +196,7 @@ function UserRoutineEditPage(){
             success: "Cargó con exito"},
             {}
             )
-        }
+        }*/
             const dismiss = () =>  toast.dismiss();
 
     return (
@@ -216,7 +226,7 @@ function UserRoutineEditPage(){
                     
 
                     <div className='row justify-content-center'>
-                        {loading == true ? load : dismiss }
+                        {showLoadingToast()}
                         <TransitionGroup component={null} className="todo-list">
                         {routine.length > 0 && routine.map((elemento, index) =>
                         <CSSTransition
