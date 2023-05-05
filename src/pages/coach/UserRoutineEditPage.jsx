fce0bf57014d995fbc33541eea3b0a0947baceab
martinlgalva <martinlgalvan@yahoo.com>
2023-05-05 17:22:21 -0300

    f

--- src/pages/coach/UserRoutineEditPage.jsx
+++ src/pages/coach/UserRoutineEditPage.jsx
@@ -15,6 +15,7 @@ import SkeletonWeek from '../../components/Skeleton/SkeletonWeek.jsx'
 
 
 import { InputSwitch } from "primereact/inputswitch";
+import { Tooltip } from 'primereact/tooltip';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -185,22 +186,33 @@ function UserRoutineEditPage(){
             </div>
 
             <article className='row justify-content-center'>
+                <div className='col-4 mx-2'>
+                    <button onClick={createWeek} className='input-group-text btn BlackBGtextWhite text-center' >Crear semana <b className='fs-6'>{weekNumber}</b></button>
 
-                <button onClick={createWeek} className='input-group-text btn BlackBGtextWhite text-center' >Crear semana <b className='fs-6'>{weekNumber}</b></button>
+                </div>
                     {routine.length > 0 &&
-                    <>
-                    <div className='mt-3 col-8'> 
-                        <InputSwitch className='custom' checked={copyWeek} onChange={(e) => setCopyWeek(e.value)} />
+                    
+                    <div className='row justify-content-center'> 
+                        <div className='col-8'>
+                            <InputSwitch checked={copyWeek} onChange={(e) => setCopyWeek(e.value)} />
+                        </div>
 
-                    </div>
-                    <div className='col-4' tooltip="Copiar automaticamente una plantilla de la última semana. ¡Podés desactivarla en cualquier momento y crear una semana de 0!" tooltipOptions={{ position: 'bottom', mouseTrack: true, mouseTrackTop: 15, style: {width: 20 + 'em'} }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                            <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
-                        </svg>
-                        <span className='displayFlexInMobile'>Copiar automaticamente una plantilla de la última semana. ¡Podés desactivarla en cualquier momento y crear una semana de 0!</span>
-                    </div>
-                    </>}
+                        <div className='col-4'>
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
+                            className="bi bi-question-circle custom-target-icon"
+                            data-pr-tooltip="No notifications"
+                            data-pr-position="right"
+                            data-pr-at="right+5 top"
+                            data-pr-my="left center-2"
+                            style={{ fontSize: '2rem', cursor: 'pointer' }} viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+                            </svg>
+                            <span className='displayFlexInMobile'>Copiar automaticamente una plantilla de la última semana. ¡Podés desactivarla en cualquier momento y crear una semana de 0!</span>
+                        </div>
+
+                    </div>}
 
 
                 <div className='col-10'>
@@ -288,6 +300,7 @@ function UserRoutineEditPage(){
                     pauseOnHover
                     theme="light"
                     />
+            <Tooltip target=".custom-target-icon" />
         </section>
     )
 }
