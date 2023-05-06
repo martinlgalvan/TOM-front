55d0fa230820a51e05c5c987a6462a5ba46d7271
martinlgalva <martinlgalvan@yahoo.com>
2023-05-05 22:31:35 -0300

    x

--- src/pages/coach/UserRoutineEditPage.jsx
+++ src/pages/coach/UserRoutineEditPage.jsx
@@ -202,9 +202,9 @@ function UserRoutineEditPage(){
                                 <Tooltip target=".custom-target-icon" />
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
                                 className="bi bi-question-circle custom-target-icon"
-                                data-pr-tooltip="No notifications"
+                                data-pr-tooltip="Copiar automaticamente una plantilla de la última semana. ¡Podés desactivarla en cualquier momento y crear una semana de 0!"
                                 data-pr-position="right"
-                                data-pr-at="right+5 top"
+                                data-pr-at="right+5"
                                 data-pr-my="left center-2"
                                 style={{ fontSize: '2rem', cursor: 'pointer' }} viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
