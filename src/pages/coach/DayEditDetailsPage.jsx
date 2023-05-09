b552d26602c5bbcc6feed69475651dbf1862c782
martinlgalva <martinlgalvan@yahoo.com>
2023-05-09 14:47:37 -0300

    c

--- src/pages/coach/DayEditDetailsPage.jsx
+++ src/pages/coach/DayEditDetailsPage.jsx
@@ -417,7 +417,7 @@ function DayEditDetailsPage(){
                                             value={sets} 
                                             onChange={(e) => handleBoolFocus(e.value)}
                                             onBlur={(e) => handleBlur(exercise_id, name, e.target.value, reps, peso, video, notas, numberExercise, valueExercise)}
-                                            autoFocus={boolFocus == 1 ? false : true}
+                                            focus={true}
                                             showButtons 
                                             buttonLayout={window.screen.width > 600 ? "horizontal" : "vertical"} 
                                             size={1} 
