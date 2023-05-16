031221477d322f6cb52439ad87f8571ed6200a1e
martinlgalva <martinlgalvan@yahoo.com>
2023-05-16 16:25:03 -0300

    c

--- src/pages/coach/DayEditDetailsPage.jsx
+++ src/pages/coach/DayEditDetailsPage.jsx
@@ -447,7 +447,7 @@ function DayEditDetailsPage(){
                                     {sets === undefined ? null :
                                     <td >
                                         <InputNumber
-                                            value={inputEnFoco !== null && inputEnFoco !== index ? newSet : sets} 
+                                            value={sets} 
                                             onChange={changeSetsEdit}
                                             onFocus={() => handleInputFocus(index)}
                                             ref={(input) => (inputRefs.current[index] = input)}
