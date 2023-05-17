e71ca50465c97beaa28fda62aa6bb6018be7d029
martinlgalva <martinlgalvan@yahoo.com>
2023-05-17 20:02:33 -0300

    v

--- src/components/Bootstrap/ModalCreateWarmup.jsx
+++ src/components/Bootstrap/ModalCreateWarmup.jsx
@@ -124,7 +124,7 @@ function changeRepsWarmup(e){
 
 
 function editWarmup(warmup_id, name, StrSets, StrReps,peso, video, notas, numberWarmup){
-
+console.log(warmup_id, name, StrSets, StrReps,peso, video, notas, numberWarmup)
 
         let sets = parseInt(StrSets)
         let reps = parseInt(StrReps)
@@ -428,7 +428,7 @@ const notify = (name) => {
 
                                 :
 
-                                <button disabled={inputEnFoco !== null && inputEnFoco !== index} onClick={(e) => editWarmup(warmup_id, newName == undefined ? name : newName, newSet == undefined ? sets : newSet, reps == undefined ? reps : newRep, peso == undefined ? peso : newPeso, video == undefined ? video : newVideo, notas == undefined ? notas : newNotas, numberWarmup)} className='btn buttonsEdit'>
+                                <button disabled={inputEnFoco !== null && inputEnFoco !== index} onClick={() => editWarmup(warmup_id, newName == undefined ? name : newName, newSet == undefined ? sets : newSet, reps == undefined ? reps : newRep, peso == undefined ? peso : newPeso, video == undefined ? video : newVideo, notas == undefined ? notas : newNotas, numberWarmup)} className='btn buttonsEdit'>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
