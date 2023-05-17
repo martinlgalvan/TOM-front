6e9e79f72ebe80963f288ee092a10b223d9ba5aa
martinlgalva <martinlgalvan@yahoo.com>
2023-05-17 04:03:06 -0300

    v

--- src/components/Bootstrap/ModalCreateWarmup.jsx
+++ src/components/Bootstrap/ModalCreateWarmup.jsx
@@ -313,17 +313,17 @@ const notify = (name) => {
                         </thead>
                         <tbody>
                         <TransitionGroup component={null} className="todo-list">
-                        {warmup != null && warmup.map((elemento, index) =>
+                        {warmup != null && warmup.map(({name,sets,reps,peso,video,notas,numberWarmup}, index) =>
                          <CSSTransition
-                         key={elemento.warmup_id}
+                         key={warmup_id}
                          timeout={500}
                          classNames="item"
                          >
-                            <tr key={elemento.warmup_id}>
+                            <tr key={warmup_id}>
                             <th scope="row">
                             <select 
-                            defaultValue={elemento.numberWarmup === null ? options[index].name : elemento.numberWarmup} 
-                            onChange={(e) => {editWarmup(elemento.warmup_id, elemento.name, elemento.sets, elemento.reps, elemento.peso, elemento.video,elemento.notas, e.target.value)}}>
+                            defaultValue={numberWarmup === null ? options[index].name : numberWarmup} 
+                            onChange={(e) => {editWarmup(warmup_id, name, sets, reps, peso, video,notas, e.target.value)}}>
                                 {options.map(option =>
                                 <optgroup key={option.value} label={option.name} >
                                     <option value={option.name}>{option.name}</option>
@@ -343,14 +343,14 @@ const notify = (name) => {
                                 id='name' 
                                 className='form-control border-0' 
                                 type="text" 
-                                defaultValue={elemento.name} 
+                                defaultValue={name} 
                                 onChange={changeNameWarmup}                                
                                 onFocus={() => handleInputFocus(index)}
                                 ref={(input) => (inputRefs.current[index] = input)}
                                 disabled={inputEnFoco !== null && inputEnFoco !== index}/>
                             </td>
                             <td><InputNumber 
-                                    value={elemento.sets} 
+                                    value={sets} 
                                     onChange={changeSetsWarmup}
                                     onValueChange={() => handleInputFocus(index)}
                                     ref={(input) => (inputRefs.current[index] = input)}
@@ -367,7 +367,7 @@ const notify = (name) => {
                             </td>
                             <td>
                                 <InputNumber 
-                                    value={elemento.reps} 
+                                    value={reps} 
                                     onChange={changeRepsWarmup}
                                     onValueChange={() => handleInputFocus(index)}
                                     ref={(input) => (inputRefs.current[index] = input)}
@@ -386,7 +386,7 @@ const notify = (name) => {
                                 <input 
                                 className='form-control border-0' 
                                 type="text" 
-                                defaultValue={elemento.peso}
+                                defaultValue={peso}
                                 onChange={changePesoWarmup}
                                 onFocus={() => handleInputFocus(index)}
                                 ref={(input) => (inputRefs.current[index] = input)}
@@ -396,7 +396,7 @@ const notify = (name) => {
                                 <input 
                                 className='form-control border-0' 
                                 type="text" 
-                                defaultValue={elemento.video}
+                                defaultValue={video}
                                 onChange={changeVideoWarmup}                                
                                 onFocus={() => handleInputFocus(index)}
                                 ref={(input) => (inputRefs.current[index] = input)}
@@ -404,13 +404,13 @@ const notify = (name) => {
                                 />
                             </td>
                             <td>
-                                <button disabled={inputEnFoco !== null && inputEnFoco !== index} onClick={(e) => editWarmup(elemento.warmup_id, name == undefined ? name : name, sets == undefined ? sets : sets, reps == undefined ? reps : reps, peso == undefined ? peso : peso, video == undefined ? video : video, notas == undefined ? notas : notas, elemento.numberWarmup)} className='btn buttonsEdit'>
+                                <button disabled={inputEnFoco !== null && inputEnFoco !== index} onClick={(e) => editWarmup(warmup_id, name == undefined ? name : name, sets == undefined ? sets : sets, reps == undefined ? reps : reps, peso == undefined ? peso : peso, video == undefined ? video : video, notas == undefined ? notas : notas, numberWarmup)} className='btn buttonsEdit'>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className=" bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                   </svg>
                                   </button>
-                                <button onClick={(e) => deleteWarmup(e,elemento.warmup_id,elemento.name) } className="btn ">
+                                <button onClick={(e) => deleteWarmup(e,warmup_id,name) } className="btn ">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" bi bi-trash3" viewBox="0 0 16 16">
                                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                                     </svg>
