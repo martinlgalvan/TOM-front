97bdf8ecb9b5535bb00ffc86f70e088d2eac43ad
martinlgalva <martinlgalvan@yahoo.com>
2024-03-27 05:16:34 -0300

    fix span button before sleep 27/03

--- src/App.jsx
+++ src/App.jsx
@@ -140,12 +140,12 @@ function App(){
 
         <>
 
-        <nav className={`navbar navbar-expand-lg ${!textColor ? "bbb" : "text-light"}`} style={{ "backgroundColor": `black` }} >
+        <nav className={`navbar navbar-expand-lg navbar-dark ${!textColor ? "bbb" : "text-light"}`} style={{ "backgroundColor": `black` }} >
             
             <div className="container-fluid ">
                 <a className="navbar-brand " href="/">TOM</a>
                 <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon "></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end  " id="navbarNav">
                     <ul className="navbar-nav text-center ">
